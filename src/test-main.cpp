80815191828662c4b5ed3f58f57ac5e189fc3925
Dano-Alien <daniele.molinari6@gmail.com>
2013-09-21 11:55:38 +0200

    before new changes

--- src/test-main.cpp
+++ src/test-main.cpp
@@ -9,6 +9,54 @@ using namespace std;
 using namespace Eigen;
 using namespace SLAM;
 
+/******************************************************************************
+ *          TEST
+ * ***************************************************************************/
+
+class Wrapper {
+public:
+    virtual VectorType F(const VectorType& Xv, const VectorType& U) = 0;
+    virtual MatrixType dF_dXv(const VectorType& Xv, const VectorType& U) = 0;
+};
+
+template<int N, int M>
+class WrapperImpl : public Wrapper {
+public:
+    static const int XvSize = N;
+    static const int USize = M;
+    
+    ////typedef
+    //vectors and matrices
+    typedef Eigen::Matrix<ScalarType, XvSize, 1> StateVectorType;
+    typedef Eigen::Matrix<ScalarType, USize, 1> InputVectorType;
+    typedef Eigen::Matrix<ScalarType, XvSize, XvSize> JacobianMatrixType;
+    //function pointers
+    typedef StateVectorType (*FType)(const StateVectorType&, const InputVectorType&);
+    typedef JacobianMatrixType (*dF_dXvType)(const StateVectorType&, const InputVectorType&);
+    
+    ////constructor
+    WrapperImpl(FType f, dF_dXvType df_dxv) : m_F(f), m_dF_dXv(df_dxv)
+        {}
+        
+    ////functions
+    VectorType F ( const VectorType& Xv, const VectorType& U ) {
+        StateVectorType xv(Xv);
+        InputVectorType u(U);
+        
+        return (*m_F)(xv, u);
+    }
+    MatrixType dF_dXv ( const VectorType& Xv, const VectorType& U ) {
+    }
+    
+private:
+    FType const m_F;
+    dF_dXvType const m_dF_dXv;
+};
+
+/******************************************************************************
+ *          
+ * ***************************************************************************/
+
 VectorType f(const VectorType& Xv, const VectorType& U)  {
     cerr << "f" << endl;
     return Xv;
@@ -22,15 +70,28 @@ MatrixType df_dxv(const VectorType& Xv, const VectorType& U)  {
     return m;
 }
 
+Vector3d typed_f(const Vector3d& Xv, const Vector2d& U)  {
+    cerr << "typed f" << endl;
+    return Xv;
+}
+
+Matrix3d typed_df_dxv(const Vector3d& Xv, const Vector2d& U)  {
+    cerr << "typed df_dxv" << endl;
+    
+    Matrix3d m;
+    m << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+    return m;
+}
+
 int main(int argc, char **argv) {
     std::cout << "Hello, world!\nThis is a big SLAM test\n" << std::endl;
     
     SLAMEngine e;
     
     VectorType v(3);
-    VectorType u(3);
+    VectorType u(2);
     v << 0, 1, 2;
-    u << 1, 2, 3;
+    u << 2, 3;
     
     MatrixType p(3, 3);
     p << 0.1, 0, 0, 0, 0.1, 0, 0, 0, 0.1;
@@ -39,66 +100,14 @@ int main(int argc, char **argv) {
     
     e.Predict(u, p);
     
+    Wrapper* w = new WrapperImpl<3, 2>(typed_f, typed_df_dxv);
+    
+    Vector3d typed_U;
+    typed_U << 100, -100, 0;
+    
+    cout << w->F(v, typed_U) << endl;
+    
+    delete w;
+    
     return 0;
 }
-
-////FAILED EXPERIMENT
-// ////test support
-// void F(Vector2d& x, const Vector2d& u) {
-//     x += u;
-// }
-// 
-// ////test class
-// class SLAM {
-//     //the pose
-// public:
-//     ////subclasses
-//     //placeholder
-//     class PosePlaceholder {
-//     public:
-//         //sizes
-//         virtual int GetPoseSize() = 0;
-//         virtual int GetInputSize() = 0;
-//         
-//         //prediction
-//         template<int InputSize>
-//         void Predict(const Matrix<ScalarType, InputSize, 1>& input);
-//     };
-//     //acutal pose
-//     template<int PoseSize, int InputSize>
-//     class Pose : public PosePlaceholder {
-//     public:
-// //         static const int poseSize = PoseSize;
-// //         static const int inputSize = InputSize;
-//         ////typedef
-//         typedef void (*FType)(Matrix<ScalarType, PoseSize, 1>&, const Matrix<ScalarType, InputSize, 1>&);
-//         typedef Matrix<ScalarType, PoseSize, 1> PoseType;
-//     private:
-// //         static const int poseSize = PoseSize;
-// //         static const int inputSize = InputSize;
-//         
-//         //data
-//         PoseType m_pose;
-//         FType const m_F;
-//     public:
-//         ////constructor
-//         Pose(PoseType starting_pose, FType f) : m_F(f), m_pose(starting_pose) {
-//             cout << "Pose constructed with PoseSize: " << PoseSize << ", InputSize: " << InputSize << endl;
-//         } 
-//         
-//         ////
-//         int GetPoseSize() { return PoseSize;}
-//         int GetInputSize() { return InputSize;}
-//         
-//     };
-//     
-//     
-//     SLAM() {}
-//     //pose
-//     template<int PoseSize, int InputSize>
-//     void addVehiclePose(const Matrix<ScalarType, PoseSize, 1>& starting_pose, void (*F)(Matrix<ScalarType, PoseSize, 1>&, const Matrix<ScalarType, InputSize, 1>&)) {
-//         cout << "PoseSize: " << PoseSize << ", InputSize: " << InputSize << endl;
-//         PosePlaceholder* pph = new Pose<PoseSize, InputSize>(starting_pose, F);
-//         delete pph;
-//     }
-// };
