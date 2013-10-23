48c40e689c2c52e387cbce052fab918f543a9349
Dano-Alien <daniele.molinari6@gmail.com>
2013-10-23 11:20:59 +0200

    tricycle added

--- SLAM/Vehicle/Tricycle.cpp
+++ SLAM/Vehicle/Tricycle.cpp
@@ -13,29 +13,29 @@ using namespace Eigen;
 
 ////model parameters
 double SLAM::Models::Tricycle::Parameters::TimeIncrement = 50e-3; //50ms
+double SLAM::Models::Tricycle::Parameters::L = 1.0; //1m
 
 ////model functions
-VectorType SLAM::Models::SimpleUnicycle::F(const VectorType& Xv, const VectorType& U) {
-    using namespace SLAM::Models::SimpleUnicycle::Parameters;
+VectorType SLAM::Models::Tricycle::F(const VectorType& Xv, const VectorType& U) {
+    using namespace SLAM::Models::Tricycle::Parameters;
 //         cout << ">> F called with Xv = (" << Xv.transpose() << "), U = (" << U.transpose() << ")" << endl;
     Vector3d res(Xv);
-    res[0] += U[0]*TimeIncrement*cos(Xv[2]+U[1]*TimeIncrement/2.0);
-    res[1] += U[0]*TimeIncrement*sin(Xv[2]+U[1]*TimeIncrement/2.0);
-    
-    res[2] += U[1]*TimeIncrement;
+    res[0] += U[0]*TimeIncrement*cos(Xv[2]+U[1]);
+    res[1] += U[0]*TimeIncrement*sin(Xv[2]+U[1]);
+    res[2] += U[0]*TimeIncrement/L*tan(U[1]);
     
     return res;
 }
-MatrixType SLAM::Models::SimpleUnicycle::dF_dXv(const VectorType& Xv, const VectorType& U) {
-    using namespace SLAM::Models::SimpleUnicycle::Parameters;
+MatrixType SLAM::Models::Tricycle::dF_dXv(const VectorType& Xv, const VectorType& U) {
+    using namespace SLAM::Models::Tricycle::Parameters;
 //         cout << ">> dF_dXv called with Xv = (" << Xv.transpose() << "), U = (" << U.transpose() << ")" << endl;
     MatrixType J(3, 3);
-    J <<    1, 0, (-U[0]*TimeIncrement*sin(Xv[2]+U[1]*TimeIncrement/2.0)),
-            0, 1, (U[0]*TimeIncrement*cos(Xv[2]+U[1]*TimeIncrement/2.0)),
+    J <<    1, 0, (-U[0]*TimeIncrement*sin(Xv[2]+U[1])),
+            0, 1, (U[0]*TimeIncrement*cos(Xv[2]+U[1])),
             0, 0, 1;
             
     return J;
 }
 
 ////model definition
-const VehicleModel SLAM::Models::SimpleUnicycleModel(SimpleUnicycle::F, SimpleUnicycle::dF_dXv);
+const VehicleModel SLAM::Models::TricycleModel(Tricycle::F, Tricycle::dF_dXv);
--- SLAM/Vehicle/Tricycle.h
+++ SLAM/Vehicle/Tricycle.h
@@ -13,13 +13,22 @@
 namespace SLAM { namespace Models { namespace Tricycle {
     ////model constants
     namespace Parameters {
+        //the time increment in the discretized model
         extern double TimeIncrement;
+        //the distance between the front and the rear wheel
+        extern double L;
     }
-    
+    /**
+     * Assuming that the following hold:
+     * Xv = (X, Y, Theta)^t
+     * U = (V, Delta)^t
+     * 
+     * Where V is the linear speed and Delta is the steering angle.
+     */
     ////model functions
     VectorType F(const VectorType& Xv, const VectorType& U);
     MatrixType dF_dXv(const VectorType& Xv, const VectorType& U);
 }
     ////model
-    extern const VehicleModel SimpleUnicycleModel;
+    extern const VehicleModel TricycleModel;
 } }
